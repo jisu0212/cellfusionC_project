7105fe804914aed33eb911dae2bf1e16e97328db
jigoo <165612433+jisu0212@users.noreply.github.com>
2024-06-26 18:09:31 +0900

    Add files via upload

--- script/index.js
+++ script/index.js
@@ -4,19 +4,15 @@ const bank_detail = document.querySelector ('.bank_detail')
 const family_detail = document.querySelector ('.family_detail')
 bank_info.addEventListener('mouseover', ()=>{
     bank_detail.style.display = 'block';
-    family_detail.style.display = 'block';
 })
 family.addEventListener('mouseover', ()=>{
     family_detail.style.display = 'block';
-    bank_detail.style.display = 'block';
 })
 bank_info.addEventListener('mouseout', ()=>{
     bank_detail.style.display = 'none';
-    family_detail.style.display = 'none';
 })
 family.addEventListener('mouseout', ()=>{
     family_detail.style.display = 'none';
-    bank_detail.style.display = 'none';
 })
 //===========================timer
 const hour = document.querySelector ('.hour')
@@ -34,3 +30,27 @@ setTimeout(function(){
 }, 59000)
 console.log(count);
 //=================================tab
+const categoryBtn = document.querySelectorAll ('.ranking_tab a')
+const contents = document.querySelectorAll ('.ranking_pdt_wrap .pdt_slide')
+for(let i = 0; i<7; i++){
+    categoryBtn[i].addEventListener('click', function(e){
+        e.preventDefault()
+        contents[0].style = 'display:none';
+        contents[1].style = 'display:none';
+        contents[2].style = 'display:none';
+        contents[3].style = 'display:none';
+        contents[4].style = 'display:none';
+        contents[5].style = 'display:none';
+        contents[6].style = 'display:none';
+        contents[i].style = 'display:block';
+        categoryBtn[0].style = 'color:#000; font-weight:400';
+        categoryBtn[1].style = 'color:#000; font-weight:400';
+        categoryBtn[2].style = 'color:#000; font-weight:400';
+        categoryBtn[3].style = 'color:#000; font-weight:400';
+        categoryBtn[4].style = 'color:#000; font-weight:400';
+        categoryBtn[5].style = 'color:#000; font-weight:400';
+        categoryBtn[6].style = 'color:#000; font-weight:400';
+        categoryBtn[i].style = 'color:#CF0A2C; font-weight:600';
+    })
+}
+//검색창
